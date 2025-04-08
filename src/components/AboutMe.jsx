3d7872efcd6133a6ca5c434534de0e90a5bbb66b
Madeleine Chance <mechance782@gmail.com>
2025-04-08 11:47:32 -0700

    Add AboutMe component and export

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,12 @@
+// Component containing basic info about me
+function About() {
+    return (
+        <div>
+            <h2>Madeleine Chance</h2>
+            <h3>Software Development Student</h3>
+            <p>Green River College - BAS expected June, 2026</p>
+        </div>
+    )
+}
+
+export default About;
