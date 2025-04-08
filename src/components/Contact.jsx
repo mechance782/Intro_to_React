ad93a51b228bc8d1e2d638ba75d897adea912e36
Madeleine Chance <mechance782@gmail.com>
2025-04-08 11:47:49 -0700

    Add contact component and export

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+// Component containing contact info and links
+function Contact() {
+    return (
+        <div>
+            <h3>Find me here:</h3>
+            <p>Call: #222-222-2222</p>
+            <p>Email: mechance782@gmail.com</p>
+            <p><a href="https://www.linkedin.com/in/madeleine-chance/">LinkedIn</a></p>
+            <p><a href="https://github.com/mechance782">Github</a></p>
+        </div>
+    )
+}
+
+export default Contact;
